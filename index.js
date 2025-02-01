ce388bc6f30dd942ba1d3de1d547cde0429fb2d6
Alex Fischman <alexdfischman@gmail.com>
2025-02-01 13:24:48 -0800

    Cube with colors

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ if (context == null) {
 const vertexShader = context.createShader(context.VERTEX_SHADER);
 context.shaderSource(vertexShader, `
 	attribute vec4 vertex;
+	attribute vec4 vertexColor;
 	uniform mat4 transform;
+	varying lowp vec4 fragmentColor;
 	void main() {
 		gl_Position = transform * vertex;
+		fragmentColor = vertexColor;
 	}
 `);
 context.compileShader(vertexShader);
@@ -23,8 +26,9 @@ if (!context.getShaderParameter(vertexShader, context.COMPILE_STATUS)) {
 
 const fragmentShader = context.createShader(context.FRAGMENT_SHADER);
 context.shaderSource(fragmentShader, `
+	varying lowp vec4 fragmentColor;
 	void main() {
-		gl_FragColor = vec4(1, 0, 1, 1);
+		gl_FragColor = fragmentColor;
 	}
 `);
 context.compileShader(fragmentShader);
@@ -43,13 +47,42 @@ if (!context.getProgramParameter(shaderProgram, context.LINK_STATUS)) {
 context.useProgram(shaderProgram);
 const programLocations = {
 	vertex: context.getAttribLocation(shaderProgram, "vertex"),
+	vertexColor: context.getAttribLocation(shaderProgram, "vertexColor"),
 	transform: context.getUniformLocation(shaderProgram, "transform"),
 };
 
-const vertices = [1, 1, -1, 1, 1, -1, -1, -1];
+const vertices = [
+	-1, -1,  1,  1, -1,  1,  1,  1,  1, -1,  1,  1,
+	-1, -1, -1, -1,  1, -1,  1,  1, -1,  1, -1, -1,
+	-1,  1, -1, -1,  1,  1,  1,  1,  1,  1,  1, -1,
+	-1, -1, -1,  1, -1, -1,  1, -1,  1, -1, -1,  1,
+	 1, -1, -1,  1,  1, -1,  1,  1,  1,  1, -1,  1,
+	-1, -1, -1, -1, -1,  1, -1,  1,  1, -1,  1, -1,
+];
 const vertexBuffer = context.createBuffer();
 context.bindBuffer(context.ARRAY_BUFFER, vertexBuffer);
 context.bufferData(context.ARRAY_BUFFER, new Float32Array(vertices), context.STATIC_DRAW);
+context.vertexAttribPointer(programLocations.vertex, 3, context.FLOAT, false, 0, 0);
+context.enableVertexAttribArray(programLocations.vertex);
+
+const indices = [
+	 0,  1,  2,  0,  2,  3,
+	 4,  5,  6,  4,  6,  7,
+	 8,  9, 10,  8, 10, 11,
+	12, 13, 14, 12, 14, 15,
+	16, 17, 18, 16, 18, 19,
+	20, 21, 22, 20, 22, 23,
+];
+const indexBuffer = context.createBuffer();
+context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, indexBuffer);
+context.bufferData(context.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), context.STATIC_DRAW);
+
+const vertexColors = vertices.map(x => Math.max(0, x));
+const vertexColorBuffer = context.createBuffer();
+context.bindBuffer(context.ARRAY_BUFFER, vertexColorBuffer);
+context.bufferData(context.ARRAY_BUFFER, new Float32Array(vertexColors), context.STATIC_DRAW);
+context.vertexAttribPointer(programLocations.vertexColor, 3, context.FLOAT, false, 0, 0);
+context.enableVertexAttribArray(programLocations.vertexColor);
 
 const load = performance.now();
 let time = performance.now();
@@ -86,12 +119,10 @@ const render = () => {
 		Matrix.perspective(w / h),
 	]));
 
-	context.bindBuffer(context.ARRAY_BUFFER, vertexBuffer);
-	context.vertexAttribPointer(programLocations.vertex, 2, context.FLOAT, false, 0, 0);
-	context.enableVertexAttribArray(programLocations.vertex);
-
 	context.clearColor(0, 0, 0, 1);
 	context.enable(context.DEPTH_TEST);
 	context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
-	context.drawArrays(context.TRIANGLE_STRIP, 0, 4);
+
+	context.bindBuffer(context.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	context.drawElements(context.TRIANGLES, indices.length, context.UNSIGNED_SHORT, 0);
 };
