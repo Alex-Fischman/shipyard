5791bb69b0f76bbaaacf326a860ee78858c762d3
Alex Fischman <alexdfischman@gmail.com>
2025-02-02 22:07:28 -0800

    Implement normals w/o correct scaling

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const frame = now => {
 window.requestAnimationFrame(frame);
 
 const camera = {
-	pos: [0, 0, 5],
+	pos: [0, 0, 10],
 	pitch: 0,
 	yaw: 0,
 };
@@ -29,14 +29,14 @@ const render = () => {
 
 	const models = [
 		Matrix.translation([ 0,  0, 0]),
-		Matrix.translation([ 2,  0, 0]),
-		Matrix.translation([-2,  0, 0]),
-		Matrix.translation([ 0,  2, 0]),
-		Matrix.translation([ 0, -2, 0]),
-	];
+		Matrix.translation([ 3,  0, 0]),
+		Matrix.translation([-3,  0, 0]),
+		Matrix.translation([ 0,  3, 0]),
+		Matrix.translation([ 0, -3, 0]),
+	].map(model => Matrix.mul(model, Matrix.rotation_z(0.001 * (time - load))));
 
 	const view = Matrix.compose([
-		Matrix.translation(Vector.neg(camera.pos)),
+		Matrix.translation(Vector.scale(-1, camera.pos)),
 		Matrix.rotation_y(-camera.yaw),
 		Matrix.rotation_x(-camera.pitch),
 	]);
@@ -52,27 +52,44 @@ const render = () => {
 		-1, -1, -1, -1, -1,  1, -1,  1,  1, -1,  1, -1,
 	];
 
+	const normals = [
+		 0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,
+		 0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,
+		 0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,
+		 0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,
+		 1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,
+		-1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0,
+	];
+
 	const vertexColors = [1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1];
+
+	const lightDirection = Vector.normalize([1, 1, 1]);
 
 	WebGL.draw({
 		vertex: `
 			gl_Position = projection * view * model * vec4(vertex, 1);
-			fragmentColor = vec4(vertexColor, 1);
+			fragmentColor = vertexColor;
+			fragmentNormal = mat3(model) * vertexNormal;
 		`,
 		fragment: `
-			gl_FragColor = fragmentColor;
+			float light = dot(normalize(fragmentNormal), lightDirection);
+			gl_FragColor.rgb = fragmentColor * light;
+			gl_FragColor.a = 1.0;
 		`,
 		attributes: {
-			vertex:      { type: "vec3", data: vertices },
-			vertexColor: { type: "vec3", data: vertexColors, divisor: 1 },
-			model:       { type: "mat4", data: models.flat(), divisor: 1 },
+			vertex:       { type: "vec3", data: vertices },
+			vertexColor:  { type: "vec3", data: vertexColors, divisor: 1 },
+			vertexNormal: { type: "vec3", data: normals },
+			model:        { type: "mat4", data: models.flat(), divisor: 1 },
 		},
 		uniforms: {
-			view:       { type: "mat4", data: view },
-			projection: { type: "mat4", data: projection },
+			view:           { type: "mat4", data: view },
+			projection:     { type: "mat4", data: projection },
+			lightDirection: { type: "vec3", data: lightDirection },
 		},
 		varyings: {
-			fragmentColor: { type: "lowp vec4" },
+			fragmentColor:  { type: "vec3" },
+			fragmentNormal: { type: "vec3" },
 		},
 		instances: models.length,
 		indices: [
